cc053c3802970f217ad104b54835a74e18cfdb2d
JulioGularte <juliogulartedev@gmail.com>
2023-02-18 21:44:10 -0300

    Add project files.

---
+++ Controllers/GuildController.cs
@@ -0,0 +1,27 @@
+﻿
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MplusGuildApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuildController : ControllerBase
+    {
+        private readonly IGuildService _guildService;
+        private readonly IMythicKeyService _mythicKeyService;
+        public GuildController(IGuildService guildService, IMythicKeyService mythicKeyService)
+        {
+            _guildService = guildService;
+            _mythicKeyService = mythicKeyService;
+        }
+        [HttpGet("mythicscore")]
+        public async Task<IActionResult> GetGuildMythicScore (string guildName)
+        {
+            return Ok(await _mythicKeyService.GetGuildMythicScore(guildName));
+        }
+    }
+}
---
+++ Controllers/MythicController.cs
@@ -0,0 +1,30 @@
+﻿using Application.ApiResponses.JSON;
+using Application.Authorization;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MplusGuildApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MythicController : ControllerBase
+    {
+        private readonly IMythicKeyService _mythicKeyService;
+        private readonly IAccessTokenService _accessTokenService;
+        public MythicController(IMythicKeyService mythicKeyService, IAccessTokenService accessTokenService)
+        {
+            _mythicKeyService = mythicKeyService;
+            _accessTokenService = accessTokenService;
+        }
+        [HttpGet("playermythicprofile")]
+        public async Task <IActionResult> GetPlayerMythicProfileAsync (string character)
+        {
+            Token token = await _accessTokenService.RequestToken();
+            return Ok(await _mythicKeyService.GetPlayerMythicProfile(character));
+
+        }
+
+    }
+}
